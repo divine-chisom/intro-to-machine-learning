f0ed67be9e9d7f90ac8edbfcde3c42feb638bb0a
divine-chisom <divinechisom1995@gmail.com>
2023-04-21 20:56:55 +0100

    added solution for Q3

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,9 +24,13 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ### your code goes here ###
 from sklearn.svm import SVC
 clf = SVC(kernel="linear")
+t0 = time()
 clf.fit(features_train, labels_train)
+print("Training Time:", round(time()-t0, 3), "s")
 
+t0 = time()
 pred = clf.predict(features_test)
+print("Predicting Time:", round(time()-t0, 3), "s")
 
 from sklearn.metrics import accuracy_score
 acc = accuracy_score(pred, labels_test)
