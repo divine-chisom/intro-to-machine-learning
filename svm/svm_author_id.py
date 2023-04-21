0389183898d3a4b180903780b80c283669735cd9
divine-chisom <divinechisom1995@gmail.com>
2023-04-21 22:29:03 +0100

    solution for Q6

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -23,7 +23,7 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 from sklearn.svm import SVC
-clf = SVC(kernel="rbf")
+clf = SVC(C=10000.)
 
 features_train = features_train[:int(len(features_train)/100)]
 labels_train = labels_train[:int(len(labels_train)/100)]
